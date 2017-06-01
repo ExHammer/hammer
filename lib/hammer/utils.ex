8ec7e02da42c710f7c52a61faa1fe50e3f4365da
Shane Kilkelly <shane@kilkelly.me>
2017-06-01 19:36:41 +0100

    Typo

--- lib/hammer/utils.ex
+++ lib/hammer/utils.ex
@@ -2,7 +2,7 @@ defmodule Hammer.Utils do
 
   # Returns Erlang Time as milliseconds since 00:00 GMT, January 1, 1970
   def timestamp do
-    Datetime.utc_now() |> DateTime.to_unix(:millisecond)
+    DateTime.utc_now() |> DateTime.to_unix(:millisecond)
   end
 
 end

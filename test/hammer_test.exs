5c2e4995aaaab23579c41cfa711ef0450293a18e
Shane Kilkelly <shane@kilkelly.me>
2017-06-03 16:58:54 +0100

    Add test for timestamp

--- test/hammer_test.exs
+++ test/hammer_test.exs
@@ -6,3 +6,14 @@ defmodule HammerTest do
     assert 1 + 1 == 2
   end
 end
+
+
+defmodule UtilsTest do
+  use ExUnit.Case
+  doctest Hammer.Utils
+
+  test "timestamp" do
+    assert is_integer(Hammer.Utils.timestamp())
+  end
+
+end

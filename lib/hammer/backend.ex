571e3f5e21f1e4d8bbddac6d00f5cbb88172ae0a
Shane Kilkelly <shane@kilkelly.me>
2017-08-14 18:54:19 +0100

    Add a `Hammer.Backend` behaviour module.

---
+++ lib/hammer/backend.ex
@@ -0,0 +1,22 @@
+defmodule Hammer.Backend do
+
+  @moduledoc """
+  The backend Behaviour module
+  """
+
+  @callback count_hit(key::{bucket::integer, id::String.t}, now::integer)
+            :: {:ok, count::integer}
+             | {:error, reason::any}
+
+  @callback get_bucket(key::{bucket::integer, id::String.t})
+             :: {:ok, {key::{bucket::integer, id::String.t},
+                       count::integer,
+                       created::integer,
+                       updated::integer}}
+              | {:ok, nil}
+              | {:error, reason::any}
+
+  @callback delete_buckets(id::String.t)
+              :: {:ok, count_deleted::integer}
+               | {:error, reason::any}
+end

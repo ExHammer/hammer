f5bd8836bb455001a57d64f50e0409e93eacbf1a
Shane Kilkelly <shane@kilkelly.me>
2017-08-28 14:07:50 +0100

    Add tests for Hammer.Backend.ETS

--- test/hammer_test.exs
+++ test/hammer_test.exs
@@ -4,7 +4,6 @@ defmodule HammerTest do
 
   setup _context do
     {:ok, _hammer_ets_pid} = Hammer.Backend.ETS.start_link()
-    # {:ok, hammer_pid} = Hammer.start_link(backend: Hammer.Backend.ETS)
     {:ok, []}
   end
 
@@ -76,6 +75,45 @@ defmodule HammerTest do
 
 end
 
+defmodule ETSTest do
+  use ExUnit.Case
+
+  setup _context do
+    {:ok, _hammer_ets_pid} = Hammer.Backend.ETS.start_link()
+    {:ok, []}
+  end
+
+  test "count_hit" do
+    {stamp, key} = Hammer.Utils.stamp_key("one", 200_000)
+    assert {:ok, 1} = Hammer.Backend.ETS.count_hit(key, stamp)
+    assert {:ok, 2} = Hammer.Backend.ETS.count_hit(key, stamp)
+    assert {:ok, 3} = Hammer.Backend.ETS.count_hit(key, stamp)
+  end
+
+  test "get_bucket" do
+    {stamp, key} = Hammer.Utils.stamp_key("two", 200_000)
+    # With no hits
+    assert {:ok, nil} = Hammer.Backend.ETS.get_bucket(key)
+    # With one hit
+    assert {:ok, 1} = Hammer.Backend.ETS.count_hit(key, stamp)
+    assert {:ok, {{_, "two"}, 1, _, _}} = Hammer.Backend.ETS.get_bucket(key)
+    # With two hits
+    assert {:ok, 2} = Hammer.Backend.ETS.count_hit(key, stamp)
+    assert {:ok, {{_, "two"}, 2, _, _}} = Hammer.Backend.ETS.get_bucket(key)
+  end
+
+  test "delete_buckets" do
+    {stamp, key} = Hammer.Utils.stamp_key("three", 200_000)
+    # With no hits
+    assert {:ok, 0} = Hammer.Backend.ETS.delete_buckets("three")
+    # With three hits in same bucket
+    assert {:ok, 1} = Hammer.Backend.ETS.count_hit(key, stamp)
+    assert {:ok, 2} = Hammer.Backend.ETS.count_hit(key, stamp)
+    assert {:ok, 3} = Hammer.Backend.ETS.count_hit(key, stamp)
+    assert {:ok, 1} = Hammer.Backend.ETS.delete_buckets("three")
+  end
+
+end
 
 defmodule UtilsTest do
   use ExUnit.Case
